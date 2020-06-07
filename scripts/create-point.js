11b2e49a1cdf0b5ae62183775f85823e32852eb1
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-06-07 18:19:04 -0300

    página create-point atualizada

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,5 +1,5 @@
 
-
+// Função para popular o campo de estados 
 function populationUFs (){
     const ufSelect = document.querySelector("[name=uf]");
 
@@ -29,6 +29,9 @@ function getCities(event){
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
+    citySelect.innerHTML = "";
+    citySelect.disabled = `<option value="">Digite sua cidade</option>;`
+
     fetch(url)
         .then(response => response.json()) //função anonima que retorna o json
         .then(cities => { //retorna os estados do json
@@ -48,3 +51,45 @@ document
     .querySelector("[name=uf]")
     //Fica esperando a mudança do campo UF do create-point, com uma arrow function para liberar as cidades
     .addEventListener("change", getCities); //quando mudar, pega a função getCities e execute
+
+//Itens de coleta
+//pegar todos os li's de uma vez só 
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect){
+    item.addEventListener("click", hadleSelectedItem);
+}
+
+const collectedItems = document.querySelector("input[name=items]");
+let selectedItens = [];
+
+function hadleSelectedItem (event) {
+
+    const itemList = event.target;
+    
+    // add ou remove any class with javascript
+    itemList.classList.toggle("selected");
+
+    const itemId = itemList.dataset.id;
+
+    // verificar se existem itens selecionados, se sim, pega os itens selecionados
+    const alreadySelected = selectedItens.findIndex(item => item == itemId); 
+    //aqui eu estou fazendo a comparação do item com o itemId, usando arrow function, só vai retornar algo quando item == itemId retornar true
+
+    // se já estiver selecionado
+    if (alreadySelected >= 0 ){
+        //se o item for diferente, ele filtra e tira do array
+        const filteredItems = selectedItens.filter(item => {
+            const itemIsDifferent = item != itemId; 
+            return itemIsDifferent; 
+        });
+    //atualiza o array sem os itens filtrados 
+    selectedItens = filteredItems; 
+    }else{
+        //adiciona o item ao array
+        selectedItens.push(itemId);
+    }
+    // atualiza o campo escondido com itens selecionados
+
+    collectedItems.value = selectedItens;
+}
