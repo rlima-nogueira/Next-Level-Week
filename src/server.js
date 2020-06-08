c6a026d2d228d03433afaccaadf777b922fb1aaa
Rafaela Lima Nogueira <rn-15@hotmail.com>
2020-06-08 18:36:15 -0300

    configurando a chamada do servidor

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const express = require("express");
 //executo a variavel que defini em cima
 const server = express();
 
+//redireciona o servidor para a home 
+server.get("/", (req, res) =>{
+    res.sendfile(__dirname + "/views/index.html");
+});
+
+
 //liga o servidor com a porta 3000 
 server.listen(3000);
 
